1d05802d949a72d6bfaa6ba7190861e8fde1faae
Tatsuno <ee227059@meiji.ac.jp>
2025-03-25 00:48:51 +0900

    フロントとバックのAPI通信

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 const PORT: number = 8080;
 
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, world!');
+    res.json({ message: 'Hello World!' });
 });
 
 app.listen(PORT, () => {
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,43 +1,23 @@
 <script setup>
-import { ref } from 'vue'
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
 
-defineProps({
-  msg: String,
-})
+  const message = ref('');
 
-const count = ref(0)
+  onMounted(async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/');
+      message.value = response.data.message;
+    } catch (error) {
+      console.error('API Error:', error);
+    }
+  });
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div>
+    <h1>Axios API 通信</h1>
+    <p>{{ message }}</p>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
