109535ca7201f572a85c6f8ed4bd7512b99a454a
Tatsuno <ee227059@meiji.ac.jp>
2025-04-02 01:16:41 +0900

    デザイン統一

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,6 +3,7 @@
     import axios from 'axios';
     import router from '../router';
 
+
     const signOut = async() => {
         try{
             await axios.post('http://localhost:8080/sign-out');
@@ -15,8 +16,36 @@
 </script>
 
 <template>
-  <div>
+  <div class="header">
     <h1>Todoリスト</h1>
     <router-link to="/" @click.prevent="signOut">サインアウト</router-link>
   </div>
 </template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #ffffff;
+  border-bottom: 1px solid #ddd;
+}
+
+.header h1 {
+  margin: 0;
+  font-size: 40px;
+  color: #333;
+}
+
+.header a {
+  text-decoration: none;
+  color: #007bff;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.header a:hover {
+  text-decoration: underline;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -12,12 +12,49 @@
       console.error('API Error:', error);
     }
   });
+
 </script>
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #2c3e50;
+  font-size: 2.0rem;
+  text-align: center;
+  margin-top: 20px;
+}
+
+
+p {
+  color: #34495e;
+  font-size: 1.2rem;
+  text-align: center;
+  margin: 10px 0;
+}
+
+a {
+  display: inline-block;
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  text-align: center;
+}
+
+a:hover {
+  background-color: #2980b9;
+}
+</style>
+
 
 <template>
-    <div>
-      <h1>ホームページ</h1>
-      <p>これは{{message}}です。</p>
-      <router-link to="/sign-in">SignInページへ</router-link>
+    <div class="home">
+      <h1>My Todoリスト</h1>
+      <p>やるべきことを可視化</p>
+      <router-link class="sign-in" to="/sign-in">サインイン</router-link>
     </div>
   </template>
